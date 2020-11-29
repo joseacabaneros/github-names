a32b378e968fa011f87a136e12ed28f41d4e85d6
Jose Antonio Cabaneros <joseacabaneros@gmail.com>
2020-11-29 21:36:52 +0100

    fix(comments): add comments to code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import uniqueRandomArray from 'unique-random-array'
 const githubNames = require('./github-names.json')
 
 const mainExport = {
-  all: githubNames,
-  random: uniqueRandomArray(githubNames),
+  all: githubNames, // all
+  random: uniqueRandomArray(githubNames), // random
 }
 
 export default mainExport
